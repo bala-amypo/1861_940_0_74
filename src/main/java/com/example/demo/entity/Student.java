3c0c431eb91ee1f1411bd72d984ef77a263b44ca
bala-amypo <bala@amypo.in>
2025-12-18 06:30:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,5 +1,8 @@
 package com.example.demo.entity;
-import jakarta
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -9,8 +12,11 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotBlank(messege="Name filled cannot be empty")
+    @Size(min=3,max=20,messege="The name must be min  and max 20")
     private String name;
+    @Email(messege="invalid email")
+    @column(unique=true)
     private String email;
     private int age;
 
